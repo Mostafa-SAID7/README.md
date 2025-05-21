d4b2160330fabbf206a301965838a281b02f3c20
Mostafa Said <m.ssaid356@gmail.com>
2025-05-21 23:30:45 +0300

    Add files via upload

---
+++ dirApp/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace REDACTED_PROJECT_NAME
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool? isAhmed = null;
+            Console.WriteLine("Are you Ahmed? : (Y,N)");
+            char result =Convert.ToChar( Console.ReadLine());
+            
+            if (result == 'Y' || result == 'y')
+            {
+                isAhmed = true;
+            }
+            else if (result == 'N' || result == 'n')
+            {
+                isAhmed = false;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter Y or N.");
+                return;
+            }
+            Console.WriteLine($"Is Ahmed: {isAhmed}");
+        }
+    }
+}
